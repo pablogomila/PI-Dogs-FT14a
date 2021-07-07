2e6bc141f71fab982adde3f3e446e45318ef2990
Pablo Gomila <pablogomila@protonmail.com>
2021-07-07 12:06:19 -0300

    updated Dog.js model

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,33 +1,34 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-    },
+
+  sequelize.define('breed', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     height: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     weight: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false,
     },
-    lifespan: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    img: {
+    life_span: {
       type: DataTypes.STRING,
-      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
     }
-  });
+  },
+    { timestamps: false }
+  );
+
+  sequelize.define('temperament', {
+    name: {
+      type: DataTypes.STRING
+    },
+  },
+    { timestamps: false }
+  );
 };
