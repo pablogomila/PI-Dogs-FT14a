17f215739739246a30efb4393ff67641b7a98acd
Pablo Gomila <pablogomila@protonmail.com>
2021-07-12 12:25:48 -0300

    modified index and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,33 @@
-import './App.css';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Index from './views/Index/Index'
+import React, { useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './views/home/Home';
+import { Nav } from './components/nav/Nav';
+import Breed from './views/breed/Breed';
+import LandingPage from './views/landingPage/LandingPage';
+import CreateBreed from './views/createBreed/CreateBreed';
+import About from './views/about/About';
 
 function App() {
+
+  const [input, setInput] = useState('')
+
   return (
-    <BrowserRouter>
-    <div className="App">
+    <div className='App'>
       <Switch>
-        <Route exact path="/" component={Index} />
+        <Route exact path="/" component={LandingPage} />
+        <Route path='/'>
+          <Nav setInput={setInput} input={input} />
+          <Route exact path="/home" render={() => <Home setInput={setInput} input={input} />} />
+          <Route exact path="/create" component={CreateBreed} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/detail/:id" render={({ match }) => (
+            <Breed id={match.params.id}></Breed>
+          )}
+          ></Route>
+        </Route>
       </Switch>
     </div>
-    </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { store } from "./store/index";
+import axios from 'axios';
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
