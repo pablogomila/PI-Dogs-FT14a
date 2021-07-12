42798a6116237d63656a938f4b535511b53f3baf
Pablo Gomila <pablogomila@protonmail.com>
2021-07-12 12:33:34 -0300

    modified landing page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { Nav } from './components/nav/Nav';
 import Breed from './views/breed/Breed';
 import LandingPage from './views/landingPage/LandingPage';
 import CreateBreed from './views/createBreed/CreateBreed';
-import About from './views/about/About';
 
 function App() {
 
@@ -19,7 +18,6 @@ function App() {
           <Nav setInput={setInput} input={input} />
           <Route exact path="/home" render={() => <Home setInput={setInput} input={input} />} />
           <Route exact path="/create" component={CreateBreed} />
-          <Route exact path="/about" component={About} />
           <Route exact path="/detail/:id" render={({ match }) => (
             <Breed id={match.params.id}></Breed>
           )}
