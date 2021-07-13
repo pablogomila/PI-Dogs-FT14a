1f98a4184eb6e6143588cc1b86e4e20a0edc8b56
Pablo Gomila <pablogomila@protonmail.com>
2021-07-13 14:20:44 -0300

    updated components

---
+++ client/src/components/create/Create.js
@@ -0,0 +1,242 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTemperaments } from '../../actions/index'
+import axios from 'axios'
+import style from './create.module.css'
+
+function validateForm(input) {
+  let errors = {}
+  if (!input.name) {
+    errors.name = 'Name is required'
+  } else {
+    errors.name = ''
+  }
+  if (!input.weight) {
+    errors.weight = 'Weight is required'
+  } else if (!/\d{1,2}-\d{1,2}/g.test(input.weight)) {
+    errors.weight = "Must be a range. e.g. '1-5'"
+  } else {
+    errors.weight = ''
+  }
+
+  if (!input.height) {
+    errors.height = 'Height is required'
+  } else if (!/\d{1,2}-\d{1,2}/g.test(input.height)) {
+    errors.height = "Must be a range. e.g. '1-5'"
+  } else {
+    errors.height = ''
+  }
+  if (!input.life_span) {
+    errors.life_span = 'Lifespan is required'
+  } else if (!/\d{1,2}-\d{1,2}/g.test(input.life_span)) {
+    errors.life_span = "Must be a range. e.g. '1-5'"
+  } else {
+    errors.life_span = ''
+  }
+  return errors
+}
+
+function Create() {
+  const [errors, setErrors] = useState({})
+  const [touched, setTouched] = useState({})
+  const [input, setInput] = useState({
+    name: '',
+    height: '',
+    weight: '',
+    life_span: '',
+    temperament: [],
+  })
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getTemperaments())
+  }, [])
+
+  const temperaments = useSelector((state) => state.temperaments)
+
+  function handleInput(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
+    setErrors(
+      validateForm({
+        ...input,
+        [e.target.name]: e.target.value,
+      }),
+    )
+  }
+
+  function onFocus(ev) {
+    setTouched({
+      ...touched,
+      [ev.target.name]: true,
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!errors.name && !errors.weight && !errors.height && !errors.life_span) {
+      axios
+        .post('/dogs', input)
+        .then((r) => {
+          alert('Your Custom Dog was added to the Database. ╰(*°▽°*)╯')
+          setInput({
+            name: '',
+            height: '',
+            weight: '',
+            life_span: '',
+            temperament: [],
+          })
+        })
+        .catch((res) => alert('Could not create Custom Dog'))
+    } else {
+      alert('Ups... there was a problem ¯_(ツ)_/¯')
+    }
+  }
+
+  function handleSelect(e) {
+    if (input.temperament.includes(parseInt(e.target.value))) {
+      alert('Please, don´t repeat yourself (•_•)')
+    } else {
+      setInput((prev) => ({
+        ...prev,
+        temperament: [...prev.temperament, parseInt(e.target.value)],
+      }))
+    }
+  }
+
+  function deleteTemp(e, t) {
+    setInput((prev) => ({
+      ...prev,
+      temperament: prev.temperament.filter((temp) => temp !== parseInt(t)),
+    }))
+  }
+
+  function getNames(arr) {
+    let names = []
+    temperaments.forEach((t) => {
+      arr.forEach((id) => {
+        if (parseInt(id) === t.id) {
+          names.push(t.name)
+        }
+      })
+    })
+    return names
+  }
+
+  return (
+    <div className={style.container}>
+      <form onSubmit={handleSubmit}>
+        <div className={style.inputsContainer}>
+          <div className={style.inputContainer}>
+            <p className={style.secondTitle}>Name</p>
+            <input
+              type="text"
+              name="name"
+              placeholder="Dog Name"
+              onChange={handleInput}
+              required="required"
+              onFocus={onFocus}
+              value={input.name}
+              className={style.input}
+            ></input>
+            {errors.name && touched.name && (
+              <p className={style.error}>{errors.name}</p>
+            )}
+          </div>
+
+          <div className={style.inputContainer}>
+            <p className={style.secondTitle}>Weight</p>
+            <input
+              type="text"
+              name="weight"
+              placeholder="Weight Range"
+              onChange={handleInput}
+              required="required"
+              onFocus={onFocus}
+              value={input.weight}
+              className={style.input}
+            ></input>
+            {errors.weight && touched.weight && (
+              <p className={style.error}>{errors.weight}</p>
+            )}
+          </div>
+
+          <div className={style.inputContainer}>
+            <p className={style.secondTitle}>Height</p>
+            <input
+              type="text"
+              name="height"
+              placeholder="Height Range"
+              onChange={handleInput}
+              required="required"
+              onFocus={onFocus}
+              value={input.height}
+              className={style.input}
+            ></input>
+            {errors.height && touched.height && (
+              <p className={style.error}>{errors.height}</p>
+            )}
+          </div>
+
+          <div className={style.inputContainer}>
+            <p className={style.secondTitle}>Life span</p>
+            <input
+              type="text"
+              name="life_span"
+              placeholder="Life span Range"
+              onChange={handleInput}
+              required="required"
+              onFocus={onFocus}
+              value={input.life_span}
+              className={style.input}
+            ></input>
+            {errors.life_span && touched.life_span && (
+              <p className={style.error}>{errors.life_span}</p>
+            )}
+          </div>
+        </div>
+
+        <div className={style.selectTempContainer}>
+          <p className={style.secondTitle}>Temperaments</p>
+          <select
+            name="temperaments"
+            onChange={(e) => handleSelect(e)}
+            className={style.select}
+            required
+            value={input.temperament}
+          >
+            <option>Choose temperament</option>
+
+            {temperaments.map((e) => (
+              <option value={e.id} key={e.id}>
+                {e.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={style.tempButtons}>
+          {input.temperament.map((t) => (
+            <p id={t} className={style.temperament}>
+              {getNames([t])}{' '}
+              <button
+                type="button"
+                className={style.tempButton}
+                onClick={(e) => deleteTemp(e, t)}
+              >
+                x
+              </button>
+            </p>
+          ))}
+        </div>
+        <button className={style.button} type="submit">
+          Create Dog
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Create
---
+++ client/src/components/filtersort/FilterSort.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import { sortBreeds, getTemperaments, orderByWeight } from '../../actions'
+import { useSelector, useDispatch } from 'react-redux';
+import style from "./filtersort.module.css";
+import { filter } from './../../actions/index';
+
+
+function FilterSort() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getTemperaments())
+    }, [])
+    
+    const temperaments = useSelector(state => state.temperaments)
+    const breeds = useSelector(state => state.allBreeds)
+
+    let [selectedTemp, setSelectedTemp] = useState('');
+    let [tempToFilterBy, setTempToFilterBy] = useState([]);
+
+    function handleClick() {
+        let filtered = [];
+        breeds.forEach((b) => {
+            b.temperaments.map(t => t.name === selectedTemp ?
+                filtered.push(b) : null
+            )
+        })
+        dispatch(filter(filtered));
+    }
+
+    function resetTemps(e) {
+        dispatch(filter([]));
+    }
+
+
+    function handleChangeTemp(e) {
+        setSelectedTemp(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTempToFilterBy([...tempToFilterBy, selectedTemp])
+    }
+
+    function handleOrder(e) {
+        dispatch(sortBreeds(e.target.value));
+    }
+
+    function handleOrderByWeight(e) {
+        dispatch(orderByWeight(e.target.value))
+    }
+
+    return (
+        <div className={style.mainContainer}>
+            <form className={style.formContainer} onSubmit={handleSubmit}>
+                <select onChange={handleChangeTemp} name="temperaments" value={selectedTemp} className={style.select}>
+                    <option>
+                        Filter Temperament
+                    </option>
+                    {temperaments.map((e) => (
+                        <option value={e.name} key={e.id}>{e.name}</option>)
+                    )}
+                </select>
+                <div className={style.btnContainer}>
+                    <button onClick={resetTemps} className={style.button} >Clear</button>
+                    <button onClick={() => handleClick()} className={style.button}>Filter</button>
+                </div>
+            </form>
+
+            <form className={style.formContainer}>
+                <p className={style.text}>Order By Weight</p>
+                <select onChange={handleOrderByWeight} className={style.select}>
+                    <option value=''>Options</option>
+                    <option value="MINMAX">Heavy to Light</option>
+                    <option value="MAXMIN">Light to Heavy</option>
+                </select>
+
+            </form>
+
+
+
+            <form className={style.formContainer}>
+                <p className={style.text}>Order By Letter</p>
+                <select onChange={handleOrder} className={style.select}>
+                    <option value=''>Options</option>
+                    <option value="ASC">A-Z</option>
+                    <option value="DESC">Z-A</option>
+                </select>
+            </form>
+        </div>
+    )
+}
+
+export default FilterSort
